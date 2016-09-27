c75deea32616b6283d660dfefd28a276268e569a
patrickjames0132 <patrickjames0132@users.noreply.github.com>
2016-09-27 12:41:18 -0500

    Add files via upload

--- Shell.c
+++ Shell.c
@@ -6,11 +6,37 @@
 #define LSH_TOK_DELIM " \t\r\n\a"
 
 /*
+  Function Delcarations for main loop of shell.
+*/
+char *lsh_read_line(void);
+char **lsh_split_line(char *line);
+int lsh_execute(char **args);
+
+/*
   Function Declarations for builtin shell commands:
- */
+*/
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
 
 int main(int argc, char **argv){
   // Load config files, if any.
@@ -21,6 +47,22 @@ int main(int argc, char **argv){
   // Perform any shutdown/cleanup.
 
   return EXIT_SUCCESS;
+}
+
+void lsh_loop(void){
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("%% ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
 }
 
 char *lsh_read_line(void){
@@ -58,8 +100,7 @@ char *lsh_read_line(void){
   }
 }
 
-char **lsh_split_line(char *line)
-{
+char **lsh_split_line(char *line){
   int bufsize = LSH_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token;
@@ -89,24 +130,56 @@ char **lsh_split_line(char *line)
   return tokens;
 }
 
-void lsh_loop(void){
-  char *line;
-  char **args;
-  int status;
+int lsh_execute(char **args){
+  int i;
 
-  do {
-    printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+  
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
 
-    free(line);
-    free(args);
-  } while (status);
+  return lsh_launch(args);
 }
 
-int lsh_launch(char **args)
-{
+/*
+  Builtin function implementations.
+*/
+int lsh_cd(char **args){
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int lsh_help(char **args){
+  int i;
+  printf("Stephen Brennan's LSH\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args){
+  return 0;
+}
+
+int lsh_launch(char **args){
   pid_t pid, wpid;
   int status;
 
@@ -128,76 +201,4 @@ int lsh_launch(char **args)
   }
 
   return 1;
-}
-
-/*
-  List of builtin commands, followed by their corresponding functions.
- */
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
-
-int lsh_num_builtins() {
-  return sizeof(builtin_str) / sizeof(char *);
-}
-
-/*
-  Builtin function implementations.
-*/
-int lsh_cd(char **args)
-{
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
-}
-
-int lsh_help(char **args)
-{
-  int i;
-  printf("Stephen Brennan's LSH\n");
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
-
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
-  }
-
-  printf("Use the man command for information on other programs.\n");
-  return 1;
-}
-
-int lsh_exit(char **args)
-{
-  return 0;
-}
-
-int lsh_execute(char **args)
-{
-  int i;
-
-  if (args[0] == NULL) {
-    // An empty command was entered.
-    return 1;
-  }
-  
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
-      return (*builtin_func[i])(args);
-    }
-  }
-
-  return lsh_launch(args);
 }
